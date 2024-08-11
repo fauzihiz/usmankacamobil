8481d6ead22a9f0475b427f43b996cc80bf4d043
fauzihiz <37166084+fauzihiz@users.noreply.github.com>
2024-08-12 03:01:46 +0700

    complete web content and all page

---
+++ landingpage_bak.js
@@ -0,0 +1,44 @@
+//carousel
+    let currentSlide = 0;
+    let startX, isDragging = false;
+
+    function moveSlide(n) {
+        const slides = document.querySelectorAll('.carousel-slide img');
+        const totalSlides = slides.length;
+        currentSlide = (currentSlide + n + totalSlides) % totalSlides;
+        updateSlidePosition();
+    }
+
+    function updateSlidePosition() {
+        const slides = document.querySelector('.carousel-slide');
+        slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        updateSlidePosition();
+
+        const carousel = document.querySelector('.carousel');
+
+// Add swipe functionality for touch devices
+        carousel.addEventListener('touchstart', (e) => {
+            startX = e.touches[0].clientX;
+            isDragging = true;
+        });
+
+        carousel.addEventListener('touchmove', (e) => {
+            if (!startX) return;
+            let moveX = e.touches[0].clientX;
+            let diffX = startX - moveX;
+
+            if (diffX > 50) {
+                moveSlide(1);
+                startX = null;
+                isDragging = false;
+            } else if (diffX < -50) {
+                moveSlide(-1);
+                startX = null;
+                isDragging = false;
+            }
+        });
+
+    });
